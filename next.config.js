d5fd2f633b26d1ee4fc8359d00a12187e4dd3876
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-11-02 14:53:02 +0700

    add env and images domains config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    IMAGE_URL: process.env.IMAGE_URL,
+  },
+  images: {
+    domains: [process.env.IMAGE_DOMAIN],
+  },
 };
 
 module.exports = nextConfig;
